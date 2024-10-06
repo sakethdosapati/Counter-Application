d6e4a2256e92933baea2dfe7a00dccee402160cc
sakethdosapati <sakethdosapati07@gmail.com>
2024-10-07 02:44:41 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            count : 0,
+        };
+    }
+    inc = ()=>{
+        this.setState({count : this.state.count+1});
+    }
+    dec = ()=>{
+        this.setState({count : this.state.count-1})
+    }
+    reset = ()=>{
+        this.setState({count : 0})
+    }
+    render(){
+        return(
+           <>
+           <h1 style={{textAlign:"center",fontSize:"100px"}}>
+            Counter App
+           </h1>
+             <div id="main">
+                <div id="sub-div1">
+                    <button onClick={this.inc}>+</button>
+                    <h2>{this.state.count}</h2>
+                    <button onClick={this.dec}>-</button>
+                </div>
+                <div id="sub-div2">
+                    <button onClick={this.reset}>reset</button>
+                </div>
+            </div>  
+           </>
+        )
+    }
 }
-
-export default App;
+export default Counter;
